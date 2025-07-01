ebfe9e29f71e764cd7a5ff995501263016d6e383
Veniklive <danilvivivi@gmail.com>
2025-07-01 14:44:48 +0300

    Fix algorithm

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 "use strict";
 
-function GCD(a, b) {
-  if (!Number.isInteger(a) || !Number.isInteger(b) || a === 0 || b === 0) {
+function greatestCommonDivisor(a, b) {
+  if (
+    !Number.isInteger(a) ||
+    !Number.isInteger(b) ||
+    a === 0 ||
+    b === 0 ||
+    a < 0 ||
+    b < 0
+  ) {
     return "Invalid input detected";
   }
-  a = Math.abs(a);
-  b = Math.abs(b);
 
-  function algorithmEuclidean(a, b) {
-    if (a !== 0 && b !== 0) {
-      if (a > b) {
-        a = a % b;
-        return algorithmEuclidean(a, b);
-      } else {
-        b = b % a;
-        return algorithmEuclidean(a, b);
-      }
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a = a % b;
     } else {
-      return a + b;
+      b = b % a;
     }
   }
-  return algorithmEuclidean(a, b);
+  return a + b;
 }
 
-console.log(GCD(12, 6)); //6
-console.log(GCD(30, 18)); // 6
-console.log(GCD(100, 25)); // 25
-console.log(GCD(7, 13)); // 1
-console.log(GCD(-12, 6)); // 6
-console.log(GCD(12.1, 6)); // Invalit input detected
-console.log(GCD("12", 6)); // Invalit input detected
-console.log(GCD(0, 6)); // Invalit input detected
-console.log(GCD(0, 0)); // Invalit input detected
+console.log(greatestCommonDivisor(12, 6)); //6
+console.log(greatestCommonDivisor(30, 18)); // 6
+console.log(greatestCommonDivisor(100, 25)); // 25
+console.log(greatestCommonDivisor(7, 13)); // 1
+console.log(greatestCommonDivisor(-12, 6)); // Invalit input detected
+console.log(greatestCommonDivisor(12.1, 6)); // Invalit input detected
+console.log(greatestCommonDivisor("12", 6)); // Invalit input detected
+console.log(greatestCommonDivisor(0, 6)); // Invalit input detected
+console.log(greatestCommonDivisor(0, 0)); // Invalit input detected
